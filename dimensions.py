5462e2331d2944ed9de9c88b7849c9a7acec36b6
Vladyslaviuss <thoughts.stream@gmail.com>
2023-03-22 20:15:43 +0200

    :art: New small snippets

--- dimensions.py
+++ dimensions.py
@@ -1,1 +1,44 @@
+class Dimensions:
+    MIN_DIMENSION = 10
+    MAX_DIMENSION = 1000
 
+    def __init__(self, a, b, c):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+
+    @classmethod
+    def __verify_value(cls, value):
+        return type(value) in (int, float) and cls.MIN_DIMENSION <= value <= cls.MAX_DIMENSION
+    @property
+    def a(self):
+        return self.__a
+
+    @a.setter
+    def a(self, value):
+        if self.__verify_value(value):
+            self.__a = value
+
+    @property
+    def b(self):
+        return self.__b
+
+    @b.setter
+    def b(self, value):
+        if self.__verify_value(value):
+            self.__b = value
+
+    @property
+    def c(self):
+        return self.__c
+
+    @c.setter
+    def c(self, value):
+        if self.__verify_value(value):
+            self.__c = value
+
+    def __setattr__(self, key, value):
+        if key in ('MIN_DIMENSION', 'MAX_DIMENSION'):
+            raise AttributeError("Менять атрибуты MIN_DIMENSION и MAX_DIMENSION запрещено.")
+
+        super().__setattr__(key, value)
